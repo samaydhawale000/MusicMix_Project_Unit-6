a786e3ff7e65b6c37b239a85e927e49556aee008
Samay Dhawale <samaydhawale1@gmail.com>
2023-07-21 23:38:50 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
+import MusicPlayBar from './component/MusicPlayBar';
 
 function App() {
   return (
     <div className="App">
-    
         <Header/>
+        <MusicPlayBar />
     </div>
   );
 }
